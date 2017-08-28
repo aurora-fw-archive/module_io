055cc07640a26218741b822c1d79291f4d8820cd
Luís Ferreira <lsferreira169@gmail.com>
2017-08-28 10:14:18 +0100

    Fix ambiguous library name

--- module.cmake
+++ module.cmake
@@ -6,8 +6,8 @@ endif()
 
 include_directories(${MODULE_IO_DIR}/include)
 
-add_library (aurora-io SHARED ${MODULE_IO_SOURCE_DIR}/InfoOS.cpp
+add_library (aurorafw-io SHARED ${MODULE_IO_SOURCE_DIR}/InfoOS.cpp
                               ${MODULE_IO_SOURCE_DIR}/InfoRAM.cpp
                               ${MODULE_IO_SOURCE_DIR}/Timer.cpp)
 
-set_target_properties(aurora-io PROPERTIES OUTPUT_NAME aurora-io)
+set_target_properties(aurorafw-io PROPERTIES OUTPUT_NAME aurorafw-io)
